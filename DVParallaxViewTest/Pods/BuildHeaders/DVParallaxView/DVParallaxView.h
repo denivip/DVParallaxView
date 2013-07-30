f43e3d76a36d8dac5bdb63845ffc2634a3919c1f
Mikhail Grushin <grushin@denivip.ru>
2013-07-30 17:09:29 +0400

    test project is now using CocoaPods

---
+++ DVParallaxViewTest/Pods/BuildHeaders/DVParallaxView/DVParallaxView.h
@@ -0,0 +1,1 @@
+../../../../DVParallaxView/DVParallaxView.h
---
+++ DVParallaxViewTest/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// DVParallaxView
+#define COCOAPODS_POD_AVAILABLE_DVParallaxView
+#define COCOAPODS_VERSION_MAJOR_DVParallaxView 0
+#define COCOAPODS_VERSION_MINOR_DVParallaxView 1
+#define COCOAPODS_VERSION_PATCH_DVParallaxView 0
+
